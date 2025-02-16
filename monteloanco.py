166c95b11a2816b2ddb2b85ca1010ad507c2028c
warner121 <warner121@hotmail.com>
2025-02-16 19:13:03 +0000

    inference working with length-batched dataset

--- monteloanco.py
+++ monteloanco.py
@@ -28,19 +28,30 @@ class GroupedBatchSampler(BatchSampler):
 
     def __len__(self):
         return len(self.batches)
-        
 
+
+def tmat_reshape(model, tmat):
+    
+    #tmat = model.linear1(tmat)
+    #tmat = model.linear2(F.relu(tmat))
+    #tmat = torch.matmul(tmat, model.weight1.T) + model.bias1
+    tmat = tmat.reshape(-1, 8, 8)
+    tmat = tmat.masked_fill(model.tmat_mask, float('-inf'))
+    tmat = F.softmax(tmat, dim=-1)
+    tmat = torch.nan_to_num(tmat, nan=0.0)
+    return tmat
+
+    
 class Model(PyroModule):
     
-    def __init__(self, input_size, embedding_size, device='cuda:0', scaling_factor=1_000_000):
+    def __init__(self, embedding_size, device='cuda:0', scaling_factor=1_000_000):
         super().__init__()
+        self.embedding_size = embedding_size
         self.device = device
         self.scaling_factor = scaling_factor # scale to make the gradients more manageable ($500 becomes 0.5 etc.)
-
-        # define the embedding and linear terms to translate embedding to transition matrix
-        self.embeddings = pyro.param("embeddings", torch.randn(input_size, 64).to(self.device))
-        self.linear1 = torch.nn.Linear(embedding_size, 64)
-        self.linear2 = torch.nn.Linear(64, 64)
+        
+        #self.linear1 = torch.nn.Linear(self.embedding_size, 64)
+        #self.linear2 = torch.nn.Linear(64, 64)
 
         # define a hand-crafted matrix for demonstration purposes only
         self.tmat_mask = torch.tensor([
@@ -63,24 +74,14 @@ class Model(PyroModule):
             [0.,    0.2,  0.,    0.,  0.,  0.2, 0.6, 0., ],
             [0.,    0.2,  0.,    0.,  0.,  0.,  0.2, 0.6,],
             [0.,    0.,   0.,    0.,  0.,  0.,  0.,  1., ],]).to(self.device)
-
-    def _idx_to_tmat(self, idx, batch_size):
-        
-        tmat = self.embeddings[idx]
-        tmat = tmat.reshape(batch_size, 8, 8)
-        tmat = tmat.masked_fill(self.tmat_mask, float('-inf'))
-        tmat = F.softmax(tmat, dim=-1)
-        tmat = torch.nan_to_num(tmat, nan=0.0)
-        
-        return tmat
     
-    def forward(self, batchidx, idx, installments, loan_amnt, int_rate, pymnts=None, num_timesteps=None, demo=False):
+    def forward(self, batch_id, batch_idx, installments, loan_amnt, int_rate, pymnts=None, num_timesteps=None, demo=False):
 
         # transpose the input tensors to make stacking/indexing slighly easier
         installments = installments / self.scaling_factor
         loan_amnt = loan_amnt / self.scaling_factor
 
-        batch_size=len(idx)
+        batch_size=len(batch_idx)
         if torch.is_tensor(pymnts): 
             pymnts = pymnts.T / self.scaling_factor
             num_timesteps = pymnts.shape[0]
@@ -96,13 +97,16 @@ class Model(PyroModule):
         sim_pymnts = torch.zeros((1, batch_size)).to(self.device)
         hidden_states = torch.ones((1, batch_size), dtype=torch.int32).to(self.device)
 
-        # fetch embeddings and convert into transition matrices
-        tmat = self._idx_to_tmat(idx, batch_size)
+        # define the embedding and linear terms to translate embedding to transition matrix
+        self.embeddings = pyro.param(f"embeddings_{batch_id}", torch.randn(batch_size, self.embedding_size).to(self.device))
+        #self.weight1 = pyro.param("model.weight1", torch.randn(64, self.embedding_size).to(self.device) * 0.1)
+        #self.bias1 = pyro.param("model.bias1", torch.randn(64).to(self.device) * 0.1)
+        tmat = tmat_reshape(self, self.embeddings)
 
         # overwrite with the demo matrix if demo
         if demo: tmat = self.tmat_demo.unsqueeze(0).repeat(batch_size, 1, 1)
         
-        with pyro.plate("batch", batch_size, dim=-1):
+        with pyro.plate(f"batch_{batch_id}", batch_size, dim=-1):
             for t in range(1, num_timesteps + 1):
 
                 # add interest to the balance
@@ -110,7 +114,7 @@ class Model(PyroModule):
                 remaining_balance += interest_payment
 
                 # perform the monte-carlo step
-                new_hidden_states = pyro.sample(f"hidden_state_{batchidx}_{t}", dist.Categorical(tmat[torch.arange(batch_size), hidden_states[t - 1]]))
+                new_hidden_states = pyro.sample(f"hidden_state_{batch_id}_{t}", dist.Categorical(tmat[batch_idx, hidden_states[t - 1]]))
                    
                 # calculate the amount that must have been paid to prompt the status update, where the loan has not been charged off, else 0
                 # e.g. a change from 3 month's delinquent up to date implies (3 - 0 + 1)
@@ -144,7 +148,7 @@ class Model(PyroModule):
                 principal_paid = torch.cat((principal_paid, principal_payment.unsqueeze(0)), dim=0)
                 
                 # Observation model (noisy measurement of hidden state)
-                if torch.is_tensor(pymnts): pyro.sample(f"obs_{batchidx}_{t}", dist.Normal(sim_pymnts[1:t].sum(0), 1_000. / self.scaling_factor),
+                if torch.is_tensor(pymnts): pyro.sample(f"obs_{batch_id}_{t}", dist.Normal(sim_pymnts[1:t].sum(0), 1_000. / self.scaling_factor),
                     obs=pymnts[0:t - 1].sum(0)) # pymnts is 1 shorter than the simulated vectors as the origin is omitted
 
         return hidden_states[1:], sim_pymnts[1:] * self.scaling_factor, interest_paid[1:] * self.scaling_factor, principal_paid[1:] * self.scaling_factor
@@ -152,10 +156,14 @@ class Model(PyroModule):
 
 class Guide(PyroModule):
     
-    def __init__(self, device='cuda:0'):
+    def __init__(self, embedding_size, device='cuda:0'):
         super().__init__()
+        self.embedding_size = embedding_size
         self.device = device
-        
+
+        #self.linear1 = torch.nn.Linear(self.embedding_size, 64)
+        #self.linear2 = torch.nn.Linear(64, 64)
+
         # define a hand-crafted matrix for demonstration purposes only
         self.tmat_mask = torch.tensor([
             [ False,  True,  True,  True,  True,  True,  True,  True, ], # [full-paid, current, 30 days late, 60 days late, ..., charged-off]
@@ -167,7 +175,7 @@ class Guide(PyroModule):
             [ False, False, False, False, False, False, False, False, ],
             [  True,  True,  True,  True,  True,  True,  True, False, ],]).to(self.device)
         
-    def forward(self, batchidx, idx, installments, loan_amnt, int_rate, pymnts):
+    def forward(self, batch_id, batch_idx, installments, loan_amnt, int_rate, pymnts):
         
         # transpose the input tensors to make stacking/indexing slighly easier
         pymnts = pymnts.T
@@ -175,23 +183,19 @@ class Guide(PyroModule):
         # determine the shape of the inputs
         num_timesteps = pymnts.shape[0]
         batch_size = pymnts.shape[1]
-    
-        with pyro.plate("batch", batch_size, dim=-1):
-    
-            # Variational parameters for the hidden states
-            tmat_prior = pyro.param(f'tmat_prior_{batchidx}',
-                dist.Normal(
-                    torch.zeros(batch_size, 64).to(self.device),
-                    torch.ones(batch_size, 64).to(self.device)))
 
-            tmat_prior = tmat_prior.reshape(batch_size, 8, 8)
-            tmat_prior = tmat_prior.masked_fill(self.tmat_mask, float('-inf'))
-            tmat_prior = F.softmax(tmat_prior, dim=-1)
-            tmat_prior = torch.nan_to_num(tmat_prior, nan=0.0)
+        # define the embedding and linear terms to translate embedding to transition matrix
+        self.embeddings = pyro.param(f"embeddings_prior_{batch_id}", torch.randn(batch_size, self.embedding_size).to(self.device))
+        #self.weight1 = pyro.param("guide.weight1", torch.randn(64, self.embedding_size).to(self.device) * 0.1)
+        #self.bias1 = pyro.param("guide.bias1", torch.randn(64).to(self.device) * 0.1)
+        tmat_prior = tmat_reshape(self, self.embeddings)
+
+        with pyro.plate(f"batch_{batch_id}", batch_size, dim=-1):
 
             # Variational posterior for the initial hidden state
             hidden_states = torch.ones(batch_size, dtype=torch.int32).to(self.device)
         
             for t in range(1, num_timesteps + 1):
+                
                 # Variational posterior for each hidden state
-                hidden_states = pyro.sample(f"hidden_state_{batchidx}_{t}", dist.Categorical(tmat_prior[torch.arange(batch_size), hidden_states]))
+                hidden_states = pyro.sample(f"hidden_state_{batch_id}_{t}", dist.Categorical(tmat_prior[batch_idx, hidden_states]))
