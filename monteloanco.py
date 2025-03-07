0f7fc2dfee437b1c13be61932e480098d9bc95cb
warner121 <warner121@hotmail.com>
2025-03-07 06:48:26 +0000

    increase obs sigma to 500

--- monteloanco.py
+++ monteloanco.py
@@ -150,7 +150,7 @@ class Model(PyroModule):
                 principal_paid = torch.cat((principal_paid, principal_payment.unsqueeze(0)), dim=0)
                 
                 # Observation model (noisy measurement of hidden state)
-                if torch.is_tensor(pymnts): pyro.sample(f"obs_{batch_id}_{t}", dist.Normal(sim_pymnts[1:t].sum(0), 100. / self.scaling_factor),
+                if torch.is_tensor(pymnts): pyro.sample(f"obs_{batch_id}_{t}", dist.Normal(sim_pymnts[1:t].sum(0), 500. / self.scaling_factor),
                     obs=pymnts[0:t - 1].sum(0)) # pymnts is 1 shorter than the simulated vectors as the origin is omitted
 
         return hidden_states[1:], sim_pymnts[1:] * self.scaling_factor, interest_paid[1:] * self.scaling_factor, principal_paid[1:] * self.scaling_factor
